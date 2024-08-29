9392eab3019a75df6cb553bfa97ebfbe186f1d1b
Theo Labadie <theo.labadie@fms-ea.com>
2024-08-29 11:52:53 +0200

    introduire une classe abstraite Ingredient (nom, quantité, unité) et deux classes filles Oeuf et chocolat

--- patisserie.py
+++ patisserie.py
@@ -3,6 +3,30 @@ import time
 import math
 from abc import ABC, abstractmethod
 
+class Ingredient(ABC):
+    def __init__(self, nom, quantite, unite):
+        self.nom = nom
+        self.quantite = quantite
+        self.unite = unite
+
+    @abstractmethod
+    def description(self):
+        pass
+
+class Oeuf(Ingredient):
+    def __init__(self, quantite):
+        Ingredient.__init__(self, "Oeuf", quantite, "unité(s)")
+
+    def description(self):
+        return f"{self.quantite} {self.unite} d'oeufs"
+
+class Chocolat(Ingredient):
+    def __init__(self, quantite):
+        Ingredient.__init__(self, "Chocolat", quantite, "grammes")
+
+    def description(self):
+        return f"{self.quantite} {self.unite} de chocolat"
+
 class Commis(ABC, threading.Thread):
     def __init__(self, nom):
         threading.Thread.__init__(self) #initialisation de la classe parente
@@ -13,22 +37,22 @@ class Commis(ABC, threading.Thread):
         pass
 
 class BatteurOeufs(Commis):
-    def __init__(self, nom, nb_oeufs):
+    def __init__(self, nom, oeufs: Oeuf):
         Commis.__init__(self, nom)
-        self.nb_oeufs = nb_oeufs
+        self.oeufs = oeufs
 
     def run(self):
         # on suppose qu'il faut 8 tours de batteur par œuf présent dans le bol
-        nb_tours = self.nb_oeufs * 8
+        nb_tours = self.oeufs.quantite * 8
         for no_tour in range(1, nb_tours + 1):
-            print(f"\t{self.nom} bat les {self.nb_oeufs} oeufs, tour n°{no_tour}")
+            print(f"\t{self.nom} bat {self.oeufs.description()}, tour n°{no_tour}")
             time.sleep(0.5)  # temps supposé d'un tour de batteur
 
 
 class FondeurChocolat(Commis):
-    def __init__(self, nom, quantite):
+    def __init__(self, nom, chocolat: Chocolat):
         Commis.__init__(self, nom)
-        self.quantite = quantite  # en grammes
+        self.chocolat = chocolat
 
     def run(self):
         print(f"{self.nom} met de l'eau à chauffer dans une bouilloire")
@@ -39,17 +63,22 @@ class FondeurChocolat(Commis):
         time.sleep(1)
         # on suppose qu'il faut 1 tour de spatule par 10 g. de chocolat
         # présent dans le bol pour faire fondre le chocolat
-        nb_tours = math.ceil(self.quantite / 10)
+        nb_tours = math.ceil(self.chocolat.quantite / 10)
         for no_tour in range(1, nb_tours + 1):
-            print(f"{self.nom} mélange {self.quantite} de chocolat à fondre, tour n°{no_tour}")
+            print(f"{self.nom} mélange {self.chocolat.description()}, tour n°{no_tour}")
             time.sleep(1)  # temps supposé d'un tour de spatule
 
 
 if __name__ == "__main__":
-    batteur = BatteurOeufs("Jonh", 6)
-    fondeur = FondeurChocolat("Jane",200)
+    oeufs = Oeuf(6)
+    chocolat = Chocolat(200)
+
+    batteur = BatteurOeufs("Jonh", oeufs)
+    fondeur = FondeurChocolat("Jane",chocolat)
+
     batteur.start()
     fondeur.start()
+
     batteur.join()
     fondeur.join()
-    print("\nJonh peux à présent incorporer le chocolat que Jane à fait fondre aux oeufs")
+    print(f"\n{batteur.nom} peux à présent incorporer le chocolat par {fondeur.nom} aux oeufs")
